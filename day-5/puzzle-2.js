30d1b85cdd677794d9a063573275ff8acce6c8c6
Toni Tienda <tonitienda@gmail.com>
2021-12-07 09:29:05 +0900

    Day-5

---
+++ day-5/puzzle-2.js
@@ -0,0 +1,68 @@
+const { getFileLines } = require('../input-tools')
+
+const main = () => {
+    const lines = getFileLines('day-5/input.txt').map(l => l.split(' -> ').map(c => c.split(',').map(Number)))
+
+   // console.log(lines)
+
+
+    const points = {}
+
+    const addPoint = (points, [x, y]) => {
+        const key = `${x}-${y}`
+        if (!points[key]) {
+            points[key] = 1
+        } else {
+            points[key]++
+        }
+    }
+
+    for (const line of lines) {
+        const [[x1, y1], [x2, y2]] = line
+
+        if (x1 === x2) {
+            const start = Math.min(y1, y2)
+            const end = Math.max(y1, y2)
+
+            for (let y = start; y <= end; y++) {
+                addPoint(points, [x1, y])
+
+            }
+        } else if (y1 === y2) {
+            const start = Math.min(x1, x2)
+            const end = Math.max(x1, x2)
+
+            for (let x = start; x <= end; x++) {
+                addPoint(points, [x, y1])
+            }
+        } else {
+            //Diagonal line. Always 45 degrees
+            const startx = Math.min(x1, x2)
+            const endx = Math.max(x1, x2)
+
+            const lineLength = endx - startx
+            
+            const slope = y1 > y2 ? -1 : 1
+            const direction = x1 > x2 ? -1 : 1
+
+
+            // console.log()
+            // console.log(`${[x1,y1]} -> ${[x2,y2]} (length: ${lineLength})`)
+            // console.log(`slope: ${slope}, direction: ${direction}`)
+            for (let i = 0; i <= lineLength; i++) {
+                const point = [x1 + (i*direction), y1 + (i* slope)]
+                // console.log(point)
+                addPoint(points, point)
+            }
+            // console.log()
+        }
+    }
+
+    // console.log(points)
+
+    const total = Object.values(points).filter(v => v > 1).length
+
+    console.log(total)
+}
+
+main()
