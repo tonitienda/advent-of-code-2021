6cbb476ba853bcbb1ef8a8754754446a4f1e7b16
Toni Tienda <tonitienda@gmail.com>
2021-12-07 19:27:45 +0900

    Day-7

---
+++ day-7/puzzle-1.js
@@ -0,0 +1,28 @@
+const { getNumbersFromFile } = require('../input-tools')
+
+const main = () => {
+    const crabPositions = getNumbersFromFile('day-7/input.txt', ',')
+
+    const min = crabPositions.reduce((min, n) => n < min ? n : min)
+    const max = crabPositions.reduce((max, n) => n > max ? n : max)
+
+    // console.log(crabPositions)
+    // console.log(min,max)
+
+    let minFuel = Infinity
+    let pos = -1
+    for(let i = min; i<=max; i++) {
+        const fuel = crabPositions.map(n => Math.abs(n - i)).reduce((sum, n) => n + sum, 0)
+
+        if(fuel < minFuel) {
+            minFuel = fuel
+            pos = i
+        }
+    }
+
+    console.log({minFuel,pos})
+
+
+}
+
+main()
